952ffb5a828d00e5e2503a26097a6363a8fd834d
accmy <iptvacc8@gmail.com>
2025-03-31 14:39:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
     
-        fetch('https://api.jsonsilo.com/public/4cbd0295-e986-48af-ac5d-6b9f9ca45d9a')
+        fetch('https://api.jsonstorage.net/v1/json/7c20db27-5dc6-4aef-b429-7f4aac595d15/70f0547b-d8d0-4f41-90ee-282fe444c693')
             .then(response => response.json())
             .then(data => {
                 document.getElementById('marquee').innerHTML = data;
